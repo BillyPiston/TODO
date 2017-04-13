27ff5b742d14173476e7adfae2b9e230ae8a6ed3
BillyPiston <oklahoma13state@gmail.com>
2017-04-13 18:32:11 +0300

    newWeb

---
+++ src/main/java/com/example/TodoUI.java
@@ -0,0 +1,43 @@
+package com.example;
+
+import com.vaadin.server.VaadinRequest;
+import com.vaadin.spring.annotation.SpringUI;
+import com.vaadin.ui.UI;
+import com.vaadin.ui.VerticalLayout;
+
+@SpringUI
+public class TodoUI extends UI{
+
+
+    private VerticalLayout root;
+
+    @Override
+    protected void init(VaadinRequest vaadinRequest) {
+        setupLayout();
+        addHeader();
+        addForm();
+        addTodoList();
+        addDeleteButton();
+    }
+
+    private void setupLayout() {
+        root = new VerticalLayout();
+    }
+
+    private void addHeader() {
+
+    }
+
+    private void addForm() {
+
+    }
+
+    private void addTodoList() {
+
+    }
+
+    private void addDeleteButton() {
+
+    }
+
+}
